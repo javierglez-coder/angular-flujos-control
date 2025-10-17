7db850ebc8d832c3e09756392c3b31fb610417e5
Jimmy Javier <jimmyjavier@gmail.com>
2025-10-17 13:18:33 -0600

    Flujo de Control básico @if

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,10 @@ export class App {
   protected readonly title = signal('angular-flujos-control'); 
   listaElementos = ['Elemento 1', 'Elemento 2', 'Elemento 3']; 
   listaItems=['Item A', 'Item B', 'Item C', 'Item D', 'Item E', 'Item F', 'Item G', 'Item H', 'Item I', 'Item J'];
+  mostrar:boolean=true;
+
+  toggleMostrar() {
+    this.mostrar = !this.mostrar;
+  }
+  
 }
