22878bddac08ae136522e7ca55ebcac86517061d
Jimmy Javier <jimmyjavier@gmail.com>
2025-10-19 12:18:25 -0600

    Flujo de Control If, Else If, Else

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,9 +11,17 @@ export class App {
   listaElementos = ['Elemento 1', 'Elemento 2', 'Elemento 3']; 
   listaItems=['Item A', 'Item B', 'Item C', 'Item D', 'Item E', 'Item F', 'Item G', 'Item H', 'Item I', 'Item J'];
   mostrar:boolean=true;
+  nivelUsuario='admin';
+  listaNivelesUsuario=['admin','editor','invitado','moderador'];
 
   toggleMostrar() {
     this.mostrar = !this.mostrar;
+  }
+
+  cambiarNivelUsuario(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    console.log(selectElement.value);
+    this.nivelUsuario = selectElement.value;
   }
   
 }
