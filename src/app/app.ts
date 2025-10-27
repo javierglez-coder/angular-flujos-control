52a5df710861453336694b9f0371a62e8b98cd6c
Jimmy Javier <jimmyjavier@gmail.com>
2025-10-27 11:27:50 -0600

    Inicio @empty

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,21 +7,10 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-flujos-control'); 
-  listaElementos = ['Elemento 1', 'Elemento 2', 'Elemento 3']; 
+  protected readonly title = signal('angular-flujos-control');   
   listaItems=['Item A', 'Item B', 'Item C', 'Item D', 'Item E', 'Item F', 'Item G', 'Item H', 'Item I', 'Item J'];
-  mostrar:boolean=true;
-  nivelUsuario='admin';
-  listaNivelesUsuario=['admin','editor','invitado','moderador'];
+  
 
-  toggleMostrar() {
-    this.mostrar = !this.mostrar;
-  }
 
-  cambiarNivelUsuario(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    console.log(selectElement.value);
-    this.nivelUsuario = selectElement.value;
-  }
   
 }
