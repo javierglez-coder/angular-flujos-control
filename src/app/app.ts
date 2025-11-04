ca650e54662a46ae026ff321c1d0e91123debb92
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-03 18:54:35 -0600

    Signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [],
+  selector: 'app-root',  
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+contador = signal(0);
+valorDoble = computed( () => this.contador() * 2 );
 
+leerValor(){
+  const valor = this.contador();
+}
+
+establecerValor(){
+  this.contador.set(0);
+}
+
+actualizarValor(valorNuevo:number){
+  this.contador.update( valorActual => valorActual + valorNuevo );
+}
   
 }
