82e9684b9843fce7789b2fd397484072fafa6bf4
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-04 21:38:25 -0600

    Inicio Pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,19 +6,6 @@ import { Component, computed, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-contador = signal(0);
-valorDoble = computed( () => this.contador() * 2 );
 
-leerValor(){
-  const valor = this.contador();
-}
-
-establecerValor(){
-  this.contador.set(0);
-}
-
-actualizarValor(valorNuevo:number){
-  this.contador.update( valorActual => valorActual + valorNuevo );
-}
   
 }
