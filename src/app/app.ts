6d4df1032628c4b3bedc85320b7b88b2f91f4a6e
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-04 22:07:52 -0600

    Pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
+import { CurrencyPipe, DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',  
   templateUrl: './app.html',
+  imports: [UpperCasePipe, LowerCasePipe, DatePipe, CurrencyPipe],
   styleUrl: './app.scss'
 })
 export class App {
-
+nombre = signal('Angular');
+fechaActual = signal(new Date());
+precio = signal(2500);
   
 }
