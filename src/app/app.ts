96c0b3d80126d4098b32a4f1dd1dfb70ab109f42
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-10 14:46:56 -0600

    Código final Effect Tema

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,21 @@ import { Component, effect, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
+//tema actual
+temaActual = signal<'light' | 'dark'>((localStorage.getItem('tema') as 'light' | 'dark') ?? 'light');
+
+constructor(){
+  //efecto para actualizar el tema
+  effect((()=>{
+    const tema = this.temaActual();
+    localStorage.setItem('tema', tema);
+    console.log('Tema guardado y actualizado en localStorage a:', tema);
+  }))
+}
+
+cambiarTema(){
+  this.temaActual.update(tema => tema === 'light' ? 'dark' : 'light');
+}
+
 
 }
