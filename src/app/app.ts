25b6be33439cf01ac54196bf476ad7f4aa98826e
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-10 13:36:59 -0600

    Effect Inicio

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,17 +7,5 @@ import { Component, effect, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-contador = signal(0);
 
-constructor(){
-  //El effect se ejecuta automáticamente cuando contador cambia
-  effect(() => {
-    console.log(`El contador ha cambiado: ${this.contador()}`);
-  });
-}
-
-incrementar(){
-  this.contador.update( valor => valor + 1 );
-}
-  
 }
