27e0b428e5022dc12db5a261bc93e47a9174af1d
Jimmy Javier <jimmyjavier@gmail.com>
2025-11-12 21:10:17 -0600

    Inicio Template Reference Variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,24 +4,8 @@ import { Component, effect, signal } from '@angular/core';
   selector: 'app-root',  
   templateUrl: './app.html',
   imports: [],
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
-//tema actual
-temaActual = signal<'light' | 'dark'>((localStorage.getItem('tema') as 'light' | 'dark') ?? 'light');
-
-constructor(){
-  //efecto para actualizar el tema
-  effect((()=>{
-    const tema = this.temaActual();
-    localStorage.setItem('tema', tema);
-    console.log('Tema guardado y actualizado en localStorage a:', tema);
-  }))
-}
-
-cambiarTema(){
-  this.temaActual.update(tema => tema === 'light' ? 'dark' : 'light');
-}
-
 
 }
